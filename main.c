69c128fbbaa3140b28a79b39a7433c707a410ce9
Samuel Couto <dev.scouto@gmail.com>
2019-05-10 19:30:58 -0300

    main added

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+    unsigned t = atoi(argv[1]); /*parametro de tempo*/
+    
+    /*chama processos gerentes...*/
+
+    return 0;
+}
